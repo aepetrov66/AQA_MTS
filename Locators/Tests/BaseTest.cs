1ad1532f699d262054cdb1c978dedec886cd6729
Anton Petrov <relatrus@gmail.com>
2024-03-03 18:50:10 +0500

    Init

---
+++ Locators/Tests/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using Locators.Core;
+
+namespace Locators.Tests;
+
+public class BaseTest
+{
+    protected IWebDriver Driver { get; set; }
+
+    [SetUp]
+    public void FactoryDriverTest()
+    {
+        Driver = new Browser().Driver;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+
+}
---
+++ SeleniumBasicHW/Helpers/Configuration/AppSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Locators.Utilites.Configuration;
+
+public class AppSettings
+{
+    public string? URL { get; set; }
+}
